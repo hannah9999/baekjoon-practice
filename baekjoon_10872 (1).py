a225ae90a63bcfa0b2824127736e148430ae5b1b
hannah9999 <85095419+hannah9999@users.noreply.github.com>
2021-07-08 21:57:03 +0900

    uploading codes

---
+++ baekjoon_10872 (1).py
@@ -0,0 +1,11 @@
+#백준 10872번
+n = int(input())
+
+def factorial(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(n))
---
+++ baekjoon_10872 (2).py
@@ -0,0 +1,4 @@
+#백준 1152번
+word = input().split()
+print(len(word))
+
